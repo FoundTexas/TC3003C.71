2849fac37da45a79d57639f58932576992457f3d
FoundTexas <rolega01@gmail.com>
2022-08-09 20:07:16 -0500

    Avances Clase

---
+++ Assets/Clase2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clase2 : MonoBehaviour
+{
+    [SerializeField] Vector3 maxScale, minScale;
+    bool isGrowing = true;
+    Vector3 curScale = new Vector3(1f,1f,1f);
+    Transform sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GameObject.Find("Sphere").transform;
+    }
+
+    void Update()
+    {
+        CheckIfGrowing();
+        curScale += new Vector3(0.1f, 0.1f, 0.1f) * (isGrowing ? 1 : -1);
+        sphere.localScale = curScale;
+    }
+
+    public void CheckIfGrowing()
+    {
+        if (curScale.x >= maxScale.x)
+        {
+            isGrowing = false;
+        }
+        else if (curScale.x <= minScale.x)
+        {
+            isGrowing = true;
+        }
+    }
+}
---
+++ Assets/Clase2_1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clase2_1 : MonoBehaviour
+{
+    Vector3 rotation;
+    Transform Cubo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cubo = GameObject.Find("Cubo").transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.x += 2 * Time.deltaTime;
+        rotation.y += 2 * Time.deltaTime;
+        rotation.z += 2 * Time.deltaTime;
+
+        Cubo.localRotation = Quaternion.Euler(rotation);
+    }
+}
